f7aa84313772cc52079241ce4ab25ac9611326c6
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-08-04 17:03:31 +0300

    minor fixes

--- handlers/commands.go
+++ handlers/commands.go
@@ -17,8 +17,10 @@ func Commands(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "понг")
 		msg.ReplyToMessageID = update.Message.MessageID
 	default:
-		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Тебе сюда https://github.com/graphql-ru/bot")
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "[github.com/graphql-ru/bot](https://github.com/graphql-ru/bot)")
 		msg.ReplyToMessageID = update.Message.MessageID
+		msg.DisableWebPagePreview = true
+		msg.ParseMode = "Markdown"
 	}
 
 	bot.Send(msg)
