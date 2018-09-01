eed6fd26270115ac078868b79ebf01b6e019e558
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-09-01 21:42:17 +0300

    minor code style fixes

--- handlers/commands.go
+++ handlers/commands.go
@@ -17,13 +17,16 @@ func Commands(bot *tgbotapi.BotAPI, update tgbotapi.Update, next func()) {
 
 	var msg tgbotapi.MessageConfig
 
+	ChatID := update.Message.Chat.ID
+	MessageID := update.Message.MessageID
+
 	switch update.Message.Command() {
 	case "ping":
-		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "pong")
-		msg.ReplyToMessageID = update.Message.MessageID
+		msg = tgbotapi.NewMessage(ChatID, "pong")
+		msg.ReplyToMessageID = MessageID
 	default:
-		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Make @graphql_bot better https://github.com/graphql-ru/bot")
-		msg.ReplyToMessageID = update.Message.MessageID
+		msg = tgbotapi.NewMessage(ChatID, "Make @graphql_bot better https://github.com/graphql-ru/bot")
+		msg.ReplyToMessageID = MessageID
 		msg.DisableWebPagePreview = true
 	}
 
