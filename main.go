f40f7211d47a82d24e70cbe7f6791771280f6540
Ruslan Tatyshev <ruslan.tatyshev@gmail.com>
2018-09-03 16:53:49 +0300

    reminder ticker 10 min

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	tgClient.Use(handlers.Commands)
 	tgClient.Use(handlers.Join)
 
-	ghClient.ReminderTicker(1*time.Hour, func(msg string) {
+	ghClient.ReminderTicker(10*time.Minute, func(msg string) {
 		telegram.ToGraphQl(tgClient.Bot, msg)
 	})
 
